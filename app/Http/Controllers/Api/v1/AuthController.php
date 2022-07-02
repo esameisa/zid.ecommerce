3f0149be253dd9833a643bd002cf54224339cf4c
esameisa <esameisa12345@gmail.com>
2022-07-02 15:56:39 +0200

    login and register functions

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+                'data' => (object) []
+            ], 401);
+        }
+
+        $token = $request->user()->createToken(auth()->user()->type);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'loged in successfully',
+            'data' => [
+                'token' => $token->plainTextToken
+            ]
+        ], 200);
+    }
+
+    // register user function
+    public function register(Request $request, string $type)
+    {
+        $request->merge(['type' => $type]);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'type' => 'required|string|max:12|in:merchant,consumer'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'type' => $request->type
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User created successfully',
+            'data' => (object) []
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'merchant',
+            'email' => 'merchant@zid.sa',
+            'type' => 'merchant',
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'consumer',
+            'email' => 'consumer@zid.sa',
+            'type' => 'consumer',
+        ]);
+    }
+}
